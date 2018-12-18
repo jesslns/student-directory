2ed31754e663e3ef026aca7e8baec4131353cfa9
jesslns <jesslns@yahoo.com.hk>
2018-12-18 16:02:40 +0800

    Allow user interaction: input student

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first names
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add student hash to the array
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of input_students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -28,6 +33,7 @@ puts "Overall, we have #{names.count} great students"
 end
 
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
